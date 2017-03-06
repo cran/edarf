4771192ef506bbd5068c84d3c992f9dd54e4f655
Zachary M. Jones <zmj@zmjones.com>
2017-03-06 07:28:57 +0000

    version 1.1.1

--- R/pd.R
+++ R/pd.R
@@ -3,7 +3,7 @@
 #' Calculates the partial dependence of the response on an arbitrary dimensional set of predictors
 #' from a fitted random forest object from the party, randomForest, randomForestSRC, or ranger packages
 #'
-#' @importFrom data.table rbindlist
+#' @importFrom data.table rbindlist setcolorder
 #' @importFrom stats predict
 #' @importFrom mmpf marginalPrediction
 #' 
@@ -32,28 +32,28 @@
 #' data(swiss)
 #' 
 #' ## classification
-#' fit <- randomForest(Species ~ ., iris)
-#' pd <- partial_dependence(fit, c("Sepal.Width", "Sepal.Length"),
+#' fit = randomForest(Species ~ ., iris)
+#' pd = partial_dependence(fit, c("Sepal.Width", "Sepal.Length"),
 #'   data = iris[, -ncol(iris)])
-#' pd_int <- partial_dependence(fit, c("Petal.Width", "Sepal.Length"), 
+#' pd_int = partial_dependence(fit, c("Petal.Width", "Sepal.Length"), 
 #'   interaction = TRUE, data = iris[, -ncol(iris)])
 #'
 #' ## Regression
-#' fit <- randomForest(Fertility ~ ., swiss)
-#' pd <- partial_dependence(fit, c("Education", "Examination"), data = swiss[, -1])
-#' pd_int <- partial_dependence(fit, c("Education", "Examination"),
+#' fit = randomForest(Fertility ~ ., swiss)
+#' pd = partial_dependence(fit, c("Education", "Examination"), data = swiss[, -1])
+#' pd_int = partial_dependence(fit, c("Education", "Examination"),
 #'   interaction = TRUE, data = swiss[, -1])
 #' @export
-partial_dependence <- function(fit, vars, n, interaction, uniform, data, ...)
+partial_dependence = function(fit, vars, n, interaction, uniform, data, ...)
   UseMethod("partial_dependence", fit)
 #' @export
-partial_dependence.randomForest <- function(fit, vars = colnames(data),
+partial_dependence.randomForest = function(fit, vars = colnames(data),
    n = c(min(nrow(unique(data[, vars, drop = FALSE])), 25L), nrow(data)), 
    interaction = FALSE, uniform = TRUE, data, ...) {
 
   ## remove target if included
-  data <- data[, !apply(data, 2, function(x) all(x == fit$y))]
-  args <- list(
+  data = data[, !apply(data, 2, function(x) all(x == fit$y))]
+  args = list(
     "data" = data,
     "vars" = vars,
     "n" = n,
@@ -65,29 +65,32 @@ partial_dependence.randomForest <- function(fit, vars = colnames(data),
     ...
   )
   
-  if (length(vars) > 1L & !interaction)
-    pd <- rbindlist(sapply(vars, function(x) {    
-      args$vars <- x
+  if (length(vars) > 1L & !interaction) {
+    pd = rbindlist(sapply(vars, function(x) {
+      args$vars = x
       renameColumns(fit, do.call("marginalPrediction", args))
     }, simplify = FALSE), fill = TRUE)
+    setcolorder(pd, c(vars, colnames(pd)[!colnames(pd) %in% vars]))
+  }
+  
   else
-    pd <- renameColumns(fit, do.call(marginalPrediction, args))
+    pd = renameColumns(fit, do.call(marginalPrediction, args))
 
-  attr(pd, "class") <- c("pd", "data.frame")
-  attr(pd, "target") <- if (is.factor(fit$y)) levels(fit$y) else ifelse(!is.null(fit$terms), deparse(attr(fit$terms, "variables")[[2]]), "prediction")
-  attr(pd, "interaction") <- interaction == TRUE
-  attr(pd, "vars") <- vars
+  attr(pd, "class") = c("pd", "data.frame")
+  attr(pd, "target") = if (is.factor(fit$y)) levels(fit$y) else ifelse(!is.null(fit$terms), deparse(attr(fit$terms, "variables")[[2]]), "prediction")
+  attr(pd, "interaction") = interaction == TRUE
+  attr(pd, "vars") = vars
   pd
 }
 #' @export
-partial_dependence.RandomForest <- function(fit, vars = colnames(data),
+partial_dependence.RandomForest = function(fit, vars = colnames(data),
   n = c(min(nrow(unique(data[, vars, drop = FALSE])), 25L), nrow(data)), 
   interaction = FALSE, uniform = TRUE, data, ...) {
 
-  target <- names(get("response", fit@data@env))
-  data <- data.frame(get("input", fit@data@env))
+  target = names(get("response", fit@data@env))
+  data = data.frame(get("input", fit@data@env))
 
-  args <- list(
+  args = list(
     "data" = data,
     "vars" = vars,
     "n" = n,
@@ -100,28 +103,29 @@ partial_dependence.RandomForest <- function(fit, vars = colnames(data),
     ...
   )
   
-  if (length(vars) > 1L & !interaction)
-    pd <- rbindlist(sapply(vars, function(x) {
-      args$vars <- x
+  if (length(vars) > 1L & !interaction) {
+    pd = rbindlist(sapply(vars, function(x) {
+      args$vars = x
       renameColumns(fit, do.call("marginalPrediction", args))
     }, simplify = FALSE), fill = TRUE)
-  else
-    pd <- renameColumns(fit, do.call(marginalPrediction, args))
+    setcolorder(pd, c(vars, colnames(pd)[!colnames(pd) %in% vars]))
+  } else
+    pd = renameColumns(fit, do.call(marginalPrediction, args))
 
-  attr(pd, "class") <- c("pd", "data.frame")
-  attr(pd, "target") <- if (ncol(fit@responses@predict_trafo) > 1) gsub(paste0(target, "\\.", collapse = "|"), "", colnames(fit@responses@predict_trafo)) else target
-  attr(pd, "interaction") <- interaction == TRUE
-  attr(pd, "vars") <- vars
+  attr(pd, "class") = c("pd", "data.frame")
+  attr(pd, "target") = if (ncol(fit@responses@predict_trafo) > 1) gsub(paste0(target, "\\.", collapse = "|"), "", colnames(fit@responses@predict_trafo)) else target
+  attr(pd, "interaction") = interaction == TRUE
+  attr(pd, "vars") = vars
   pd
 }
 #' @export
-partial_dependence.rfsrc <- function(fit, vars = colnames(data),
+partial_dependence.rfsrc = function(fit, vars = colnames(data),
   n = c(min(nrow(unique(data[, vars, drop = FALSE])), 25L), nrow(data)), 
   interaction = FALSE, uniform = TRUE, data, ...) {
 
-  target <- fit$yvar.names
+  target = fit$yvar.names
 
-  args <- list(
+  args = list(
     "data" = fit$xvar,
     "vars" = vars,
     "n" = n,
@@ -134,50 +138,40 @@ partial_dependence.rfsrc <- function(fit, vars = colnames(data),
     ...
   )
   
-  if (length(vars) > 1L & !interaction)
-    pd <- rbindlist(sapply(vars, function(x) {
-      args$vars <- x
+  if (length(vars) > 1L & !interaction) {
+    pd = rbindlist(sapply(vars, function(x) {
+      args$vars = x
       renameColumns(fit, do.call("marginalPrediction", args))
     }, simplify = FALSE), fill = TRUE)
-  else
-    pd <- renameColumns(fit, do.call(marginalPrediction, args))
+    setcolorder(pd, c(vars, colnames(pd)[!colnames(pd) %in% vars]))
+  } else
+    pd = renameColumns(fit, do.call(marginalPrediction, args))
 
-  attr(pd, "class") <- c("pd", "data.frame")
-  attr(pd, "interaction") <- interaction == TRUE
-  attr(pd, "target") <- if (is.factor(fit$yvar)) levels(fit$yvar) else fit$yvar.names
-  attr(pd, "vars") <- vars
+  attr(pd, "class") = c("pd", "data.frame")
+  attr(pd, "interaction") = interaction == TRUE
+  attr(pd, "target") = if (is.factor(fit$yvar)) levels(fit$yvar) else fit$yvar.names
+  attr(pd, "vars") = vars
   pd
 }
 
 #' @export
-partial_dependence.ranger <- function(fit, vars = colnames(data),
+partial_dependence.ranger = function(fit, vars = colnames(data),
   n = c(min(nrow(unique(data[, vars, drop = FALSE])), 25L), nrow(data)),
   interaction = FALSE, uniform = TRUE, data, ...) {
 
-  target <- names(data)[!names(data) %in% fit$forest$independent.variable.names]
-
-  if (fit$treetype == "Classification") {
-    levels <- seq_len(length(unique(data[[target]])))
-    labels <- levels(data[[target]])
-  }
+  target = names(data)[!names(data) %in% fit$forest$independent.variable.names]
 
   predict.fun = function(object, newdata) {
-    out = apply(predict(object, newdata, predict.all = TRUE)$predictions, 1,
-      function(x) {
-        if (object$treetype == "Classification") {
-          table(factor(x, levels, labels)) / length(x)
-        } else {
-          mean(x)
-        }
-      })
-
-    if (is.matrix(out))
-      t(out)
-    else
-      out
+    if (object$treetype != "Classification") {
+      predict(object, data = newdata)$predictions
+    } else {
+      t(apply(predict(object, data = newdata, predict.all = TRUE)$predictions, 1,
+        function(x) table(factor(x, seq_len(length(unique(data[[target]]))),
+          levels(data[[target]]))) / length(x)))
+      }
   }
 
-  args <- list(
+  args = list(
     "data" = data,
     "vars" = vars,
     "n" = n,
@@ -187,24 +181,24 @@ partial_dependence.ranger <- function(fit, vars = colnames(data),
     ...
   )
   
-  if (length(vars) > 1L & !interaction)
-    pd <- rbindlist(sapply(vars, function(x) {
-      args$vars <- x
-      mp <- do.call("marginalPrediction", args)
+  if (length(vars) > 1L & !interaction) {
+    pd = rbindlist(sapply(vars, function(x) {
+      args$vars = x
+      mp = do.call("marginalPrediction", args)
       if (fit$treetype == "Regression")
-        names(mp)[1] <- target
-      renameColumns(fit, mp)
+        names(mp)[ncol(mp)] = target
+      mp
     }, simplify = FALSE), fill = TRUE)
-  else {
-    mp <- do.call("marginalPrediction", args)
+    setcolorder(pd, c(vars, colnames(pd)[!colnames(pd) %in% vars]))
+  } else {
+    pd = do.call("marginalPrediction", args)
     if (fit$treetype == "Regression")
-      names(mp)[1] <- target
-    pd <- renameColumns(fit, mp)
+      names(pd)[ncol(pd)] = target
   }
 
-  attr(pd, "class") <- c("pd", "data.frame")
-  attr(pd, "interaction") <- interaction == TRUE
-  attr(pd, "target") <- if (fit$treetype == "Classification") levels(fit$predictions) else target
-  attr(pd, "vars") <- vars
+  attr(pd, "class") = c("pd", "data.frame")
+  attr(pd, "interaction") = interaction == TRUE
+  attr(pd, "target") = if (fit$treetype != "Classification") target else levels(fit$predictions)
+  attr(pd, "vars") = vars
   pd
 }
